a382a6cd84bd1e20e7b131cffd71c20e9798424c
Migue1Andrade <mfrancadeandrade@gmail.com>
2025-02-12 16:11:35 -0300

    refactor database configuration for improved security; use environment variables for sensitive information

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 module.exports = {
-	dialect: 'postgres',
-	host: 'localhost',
-	username: 'postgres',
-	password: 'pass123',
-	database: 'sqlnode',
+	dialect: process.env.DB_DIALECT,
+	host: process.env.DB_HOST,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
 	define: {
 		timestamps: true,
 		undescored: true,
-	},
+	}
 };
